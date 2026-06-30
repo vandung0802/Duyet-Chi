31559b0779ec88b1cbc85015911936f6240abc42
vandung0802 <vandung0802@gmail.com>
2026-06-30 08:40:32 +0700

    fix: them VERSION vao sw.js de browser nhan ra SW moi moi lan deploy

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+const VERSION = '20260630-1';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
