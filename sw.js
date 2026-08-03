4916dc05fb5b468f3bcd20915bb3042457584678
vandung0802 <vandung0802@gmail.com>
2026-08-03 07:07:42 +0700

    phat hanh v124: bump version.txt + sw (D da duyet buoc 1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '20260803-123';
+const VERSION = '20260803-124';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim().then(() => {
